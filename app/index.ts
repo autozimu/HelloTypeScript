beb38e3da8ed9078bb8c742e9d2742b30ef4b61d
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-03-16 20:51:26 -0400

    Setup webpack.

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,5 @@
-import greet = require('./components/component');
+import greet = require('./component');
+require('!style!css!./main.css');
 
 const app = document.createElement('div');
 document.body.appendChild(app);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+}
+
+const common = {
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: PATHS.app
+            },
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                include: PATHS.app
+            }
+        ]
+    }
+};
+
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devServer: {
+            contentBase: PATHS.build,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'error-only',
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        devtool: 'eval-source-map',
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
+}
