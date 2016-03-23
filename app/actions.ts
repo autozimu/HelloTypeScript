b2fc99bf40293482c15ceb988e54b27518905091
Jeff Li <junfengl@ca.ibm.com>
2016-03-23 17:16:01 -0400

    Lower case in action type constants for readability.

--- app/actions.ts
+++ app/actions.ts
@@ -1,27 +1,17 @@
-/*
- * action types
- */
-
-export const ADD_NOTE= 'ADD_NOTE';
-export const UPDATE_NOTE = 'UPDATE_NOTE';
-export const DELETE_NOTE = 'DELETE_NOTE';
-
-/*
- * action creators
- */
-
 export interface IAction {
     type: string;
     id: string;
     task: string;
 }
 
+export const ADD_NOTE= 'add note';
 export function addNote() {
     return {
         type: ADD_NOTE
     };
 }
 
+export const UPDATE_NOTE = 'update note';
 export function updateNote(id: string, task: string) {
     return {
         type: UPDATE_NOTE,
@@ -30,6 +20,7 @@ export function updateNote(id: string, task: string) {
     }
 }
 
+export const DELETE_NOTE = 'delete note';
 export function deleteNote(id: string) {
     return {
         type: DELETE_NOTE,
