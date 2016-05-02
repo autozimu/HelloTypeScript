40e21efbda8c2bcf3c785554f27b40766a0c1e39
Jeff Li <junfengl@ca.ibm.com>
2016-05-02 11:53:00 -0400

    Separate app and vendor.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const pkg = require('./package.json');
+
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
     app: path.join(__dirname, 'app'),
@@ -15,7 +17,7 @@ const common = {
     },
     output: {
         path: PATHS.build,
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     resolve: {
         extensions: ['', '.ts', '.tsx', '.js', '.jsx']
@@ -65,9 +67,16 @@ if (TARGET === 'start' || !TARGET) {
 
 if (TARGET === 'build') {
     module.exports = merge(common, {
+        entry: {
+            vendor: Object.keys(pkg.dependencies)
+        },
         plugins: [
             new webpack.DefinePlugin({
                 'process.env.NODE_ENV': '"production"'
+            }),
+            // Extract vendor and manifest files
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['vendor', 'manifest']
             }),
             new webpack.optimize.UglifyJsPlugin({
                 compress: {
