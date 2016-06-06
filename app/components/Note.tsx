49cf0588c2a74285f49102db503e4ce75b9027ee
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-06-05 20:46:12 -0400

    Transform Note to unmanaged component.
    To fix cases that note task was not updated when finish editing.

--- app/components/Note.tsx
+++ app/components/Note.tsx
@@ -1,37 +1,21 @@
 import * as React from 'react';
-import {Dispatch} from "redux";
-import {connect} from "react-redux";
-
-import {Editable} from './Editable';
-import {INote} from "../models/INote";
-import {deleteNote, updateNote} from "../actions/noteActions";
 
 interface Props {
-    note: INote;
-    dispatch?: Dispatch;
+    children?: any;
 }
 
-class NoteComponent extends React.Component<Props, {}> {
+export class Note extends React.Component<Props, {}> {
     constructor(props: Props) {
         super(props);
     }
     
     render() {
-        const {note} = this.props;
-        const dispatch = this.props.dispatch!;
+        const {children} = this.props; 
         
         return (
             <div className="note">
-                <Editable value={note.task}
-                          onUpdate={(task) => dispatch(updateNote(note.id, task))}
-                />
-                <button className="delete"
-                        onClick={() => dispatch(deleteNote(note.id))}>
-                    x
-                </button>
+                {children}
             </div>
         );
     }
 }
-
-export const Note = connect()(NoteComponent);
--- app/components/Notes.tsx
+++ app/components/Notes.tsx
@@ -1,9 +1,11 @@
-import * as React from "react";
-import {Dispatch} from "redux";
-import {connect} from "react-redux";
+import * as React from 'react';
+import {Dispatch} from 'redux';
+import {connect} from 'react-redux';
 
 import {Note} from './Note';
-import {INote} from "../models/INote";
+import {INote} from '../models/INote';
+import {updateNote, deleteNote} from '../actions/noteActions';
+import {Editable} from './Editable';
 
 interface Props {
     notes: INote[];
@@ -16,11 +18,20 @@ class NotesComponent extends React.Component<Props, {}> {
     }
 
     render() {
+        const dispatch = this.props.dispatch!;
         
         return (
             <ul className="notes">{this.props.notes.map(note =>
                 <li key={note.id}>
-                    <Note note={note} />
+                    <Note>
+                        <Editable value={note.task}
+                                  onUpdate={(task) => dispatch(updateNote(note.id, task))}
+                        />
+                        <button className="delete"
+                                onClick={() => dispatch(deleteNote(note.id))}>
+                            x
+                        </button>
+                    </Note>
                 </li>
             )}</ul>
         );
