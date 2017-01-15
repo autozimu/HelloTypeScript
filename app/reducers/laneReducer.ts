81f22334167449c70d8edb0ecf9f54a904846241
Junfeng Li <autozimu@gmail.com>
2017-01-15 18:22:29 -0500

    Remove dependent on redux-actions.

--- app/reducers/laneReducer.ts
+++ app/reducers/laneReducer.ts
@@ -1,7 +1,7 @@
-import {handleActions} from "redux-actions";
 import * as update from 'react-addons-update';
 
-import {ILane} from "../models/ILane";
+import { ILane } from "../models/ILane";
+import { IAction } from "./../actions/IAction";
 import {
     CREATE_LANE, ICreateLaneAction,
     UPDATE_LANE, IUpdateLaneAction,
@@ -11,77 +11,82 @@ import {
     MOVE, IMoveAction
 } from "../actions/laneActions";
 
-export const laneReducer = handleActions({
-    [CREATE_LANE]: function (lanes: ILane[], action: ICreateLaneAction): ILane[] {
-        const {lane} = action.payload;
-        
-        return lanes.concat(lane);
-    },
-
-    [UPDATE_LANE]: function (lanes: ILane[], action: IUpdateLaneAction): ILane[] {
-        const {id, name} = action.payload;
-        
-        return lanes.map(lane => {
-            if (lane.id === id) {
-                lane.name = name;
-            }
-            return lane;
-        });
-    },
-
-    [DELETE_LANE]: function (lanes: ILane[], action: IDeleteLaneAction): ILane[] {
-        const {id} = action.payload;
-        
-        return lanes.filter(lane => lane.id !== id);
-    },
-
-    [ATTACH_TO_LANE]: function (lanes: ILane[], action: IAttachToLaneAction): ILane[] {
-        const {laneId, noteId} = action.payload;
-
-        return lanes.map(lane => {
-            if (lane.noteIds.indexOf(noteId) > -1) {
-                lane.noteIds = lane.noteIds.filter(id => id !== noteId);
-            }
-            if (lane.id === laneId) {
-                lane.noteIds = lane.noteIds.concat(noteId);
-            }
-            return lane;
-        });
-    },
-
-    [DETACH_FROM_LANE]: function (lanes: ILane[], action: IDetachFromLaneAction): ILane[] {
-        const {laneId, noteId} = action.payload;
-        
-        return lanes.map(lane => {
-            if (lane.id === laneId) {
-                lane.noteIds = lane.noteIds.filter(id => id != noteId);
-            }
-            return lane;
-        });
-    },
-    
-    [MOVE]: function (lanes: ILane[], action: IMoveAction): ILane[] {
-        const {sourceId, targetId} = action.payload;
-        const sourceLane = lanes.filter(lane => lane.noteIds.indexOf(sourceId) > -1)[0];
-        const targetLane = lanes.filter(lane => lane.noteIds.indexOf(targetId) > -1)[0];
-        const sourceNoteIndex = sourceLane.noteIds.indexOf(sourceId);
-        const targetNoteIndex = targetLane.noteIds.indexOf(targetId);
-        
-        if (sourceLane === targetLane) {
-            sourceLane.noteIds = update(sourceLane.noteIds, {
-                $splice: [
-                    [sourceNoteIndex, 1],
-                    [targetNoteIndex, 0, sourceId]
-                ]
-            });
-        } else {
-            sourceLane.noteIds.splice(sourceNoteIndex, 1);
-            targetLane.noteIds.splice(targetNoteIndex, 0, sourceId);
-        }
-
-        return lanes;
+const handlers = {};
+export function laneReducer(state: Array<ILane> = [], action: IAction) {
+    if (handlers[action.type]) {
+        return handlers[action.type](state, action);
+    } else {
+        console.warn("No action handler found!");
+        return state;
     }
-    
-    
-}, []);
+}
 
+handlers[CREATE_LANE] = function (lanes: ILane[], action: ICreateLaneAction): ILane[] {
+    const {lane} = action.payload;
+
+    return lanes.concat(lane);
+};
+
+handlers[UPDATE_LANE] = function (lanes: ILane[], action: IUpdateLaneAction): ILane[] {
+    const {id, name} = action.payload;
+
+    return lanes.map(lane => {
+        if (lane.id === id) {
+            lane.name = name;
+        }
+        return lane;
+    });
+};
+
+handlers[DELETE_LANE] = function (lanes: ILane[], action: IDeleteLaneAction): ILane[] {
+    const {id} = action.payload;
+
+    return lanes.filter(lane => lane.id !== id);
+};
+
+handlers[ATTACH_TO_LANE] = function (lanes: ILane[], action: IAttachToLaneAction): ILane[] {
+    const {laneId, noteId} = action.payload;
+
+    return lanes.map(lane => {
+        if (lane.noteIds.indexOf(noteId) > -1) {
+            lane.noteIds = lane.noteIds.filter(id => id !== noteId);
+        }
+        if (lane.id === laneId) {
+            lane.noteIds = lane.noteIds.concat(noteId);
+        }
+        return lane;
+    });
+};
+
+handlers[DETACH_FROM_LANE] = function (lanes: ILane[], action: IDetachFromLaneAction): ILane[] {
+    const {laneId, noteId} = action.payload;
+
+    return lanes.map(lane => {
+        if (lane.id === laneId) {
+            lane.noteIds = lane.noteIds.filter(id => id != noteId);
+        }
+        return lane;
+    });
+};
+
+handlers[MOVE] = function (lanes: ILane[], action: IMoveAction): ILane[] {
+    const {sourceId, targetId} = action.payload;
+    const sourceLane = lanes.filter(lane => lane.noteIds.indexOf(sourceId) > -1)[0];
+    const targetLane = lanes.filter(lane => lane.noteIds.indexOf(targetId) > -1)[0];
+    const sourceNoteIndex = sourceLane.noteIds.indexOf(sourceId);
+    const targetNoteIndex = targetLane.noteIds.indexOf(targetId);
+
+    if (sourceLane === targetLane) {
+        sourceLane.noteIds = update(sourceLane.noteIds, {
+            $splice: [
+                [sourceNoteIndex, 1],
+                [targetNoteIndex, 0, sourceId]
+            ]
+        });
+    } else {
+        sourceLane.noteIds.splice(sourceNoteIndex, 1);
+        targetLane.noteIds.splice(targetNoteIndex, 0, sourceId);
+    }
+
+    return lanes;
+};
--- app/reducers/noteReducer.ts
+++ app/reducers/noteReducer.ts
@@ -1,26 +1,34 @@
-import {handleActions} from "redux-actions";
-import {INote} from "./../models/INote";
+import { INote } from "./../models/INote";
+import { IAction } from "./../actions/IAction";
 import {
     CREATE_NOTE, ICreateNoteAction,
     DELETE_NOTE, IUpdateNoteAction,
     UPDATE_NOTE, IDeleteNoteAction
 } from "./../actions/noteActions";
 
-export const noteReducer = handleActions({
-    [CREATE_NOTE]: function (notes: INote[], action: ICreateNoteAction): INote[] {
-        return notes.concat(action.payload);
-    },
+const handlers = {};
+export function noteReducer(state: Array<INote> = [], action: IAction) {
+    if (handlers[action.type]) {
+        return handlers[action.type](state, action);
+    } else {
+        console.warn('no action handler found!');
+        return state;
+    }
+}
 
-    [UPDATE_NOTE]: function (notes: INote[], action: IUpdateNoteAction): INote[] {
-        return notes.map(note => {
-            if (note.id === action.payload.id) {
-                note.task = action.payload.task;
-            }
-            return note;
-        });
-    },
+handlers[CREATE_NOTE] = function (notes: INote[], action: ICreateNoteAction): INote[] {
+    return notes.concat(action.payload);
+};
 
-    [DELETE_NOTE]: function (notes: INote[], action: IDeleteNoteAction): INote[] {
-        return notes.filter(note => note.id !== action.payload.id);
-    },
-}, []);
+handlers[UPDATE_NOTE] = function (notes: INote[], action: IUpdateNoteAction): INote[] {
+    return notes.map(note => {
+        if (note.id === action.payload.id) {
+            note.task = action.payload.task;
+        }
+        return note;
+    });
+};
+
+handlers[DELETE_NOTE] = function (notes: INote[], action: IDeleteNoteAction): INote[] {
+    return notes.filter(note => note.id !== action.payload.id);
+};
